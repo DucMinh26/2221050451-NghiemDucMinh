6dfdbaace5b4837c2123f143ca80fb7d30353a00
2221050451-NghiemDucMinh <minh2632004@gmail.com>
2026-03-10 09:20:29 +0700

    Bài thực hành Số 6:CRUD

--- BT_NET/Controllers/StudentController.cs
+++ BT_NET/Controllers/StudentController.cs
@@ -1,20 +1,52 @@
+using BT_NET.Data;
 using BT_NET.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BT_NET.Controllers{
-    public class StudentController : Controller
+namespace BT_NET.Controllers
+{
+    public class StudentController(ApplicationDbContext context) : Controller
     {
+
+        private readonly ApplicationDbContext _context = context;
+
+        //1. Hiển thị form nhập liệu
         [HttpGet]
         public IActionResult Index()
+        {
+            //Lấy ds sinh viên từ cơ sở dữ liệu
+            var students = _context.Students.ToList();
+            return View(students);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
 
+        //2. Nhận dữ liệu từ form và lưu vào SQlite
         [HttpPost]
-        public IActionResult Index(String Fullname, String StudentCode)
+        public IActionResult Create(Student std)
         {
-            ViewBag.ThongBao = "HELLO " + Fullname + " - " + StudentCode;
-            return View();
+            _context.Students.Add(std);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //1. Hiển thị form với dữ liệu cũ
+        public IActionResult Edit(string id)
+        {
+            var student = _context.Students.Find(id);
+            if (student == null) return NotFound();
+            return View(student);
+        }
+
+        //2. Nhận dữ liệu mới và cập nhật
+        [HttpPost]
+        public IActionResult Edit(Student std)
+        {
+            _context.Students.Update(std);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
--- BT_NET/Models/Student.cs
+++ BT_NET/Models/Student.cs
@@ -2,11 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BT_NET.Models
 {
-    public class Student{
+    public class Student
+    {
         [Key]
-        public string StudentCode{get; set;} = default!;
-        public string? Fullname{get; set;} 
-        public int? Age { get; set; }
-    
+        public string StudentCode { get; set; } = default!;
+        public string? FullName { get; set; }
+
     }
 }
